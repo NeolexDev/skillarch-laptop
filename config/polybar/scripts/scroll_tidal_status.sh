9fab81fa94d5659d547277611b2dd339233bad4a
Neolex <hascoet.kevin@gmal.com>
2026-03-02 09:51:52 +0100

    typechange polybar tidal scripts

--- config/polybar/scripts/scroll_tidal_status.sh
+++ config/polybar/scripts/scroll_tidal_status.sh
@@ -1,1 +1,13 @@
-/home/neolex/.config/polybar/scripts/scroll_tidal_status.sh
+#!/bin/bash
+
+# see man zscroll for documentation of the following parameters
+zscroll -l 100 \
+        --delay 0.1 \
+        --scroll-padding "  " \
+        --match-command "`dirname $0`/tidal.sh status" \
+        --match-text "playing" "--scroll 1" \
+        --match-text "paused" "--scroll 0" \
+        --update-check true "`dirname $0`/tidal.sh info" &
+
+wait
+
